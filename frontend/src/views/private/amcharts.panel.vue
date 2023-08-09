fee01f40163913da7cff6c5b03d4fb141cc4e2b4
zdm <zdm@softvisio.net>
2023-08-09 12:30:53 +0300

    fix: amcharts

--- frontend/src/views/private/amcharts.panel.vue
+++ frontend/src/views/private/amcharts.panel.vue
@@ -32,44 +32,11 @@ export default {
             );
 
             chart.children.push( am5.Label.new( root, {
-                "text": this.l10nd( "vue-ext", "Calls for the last 60 minutes" ),
+                "text": "Chart title",
                 "fontSize": 12,
                 "x": am5.percent( 50 ),
                 "centerX": am5.percent( 50 ),
             } ) );
-
-            const xAxis = chart.xAxes.push( am5xy.DateAxis.new( root, {
-                "baseInterval": {
-                    "timeUnit": "minute",
-                    "count": 1,
-                },
-                "renderer": am5xy.AxisRendererX.new( root, {} ),
-                "tooltipDateFormat": "HH:mm",
-                "tooltip": am5.Tooltip.new( root, {} ),
-            } ) );
-
-            const yAxis = chart.yAxes.push( am5xy.ValueAxis.new( root, {
-                "renderer": am5xy.AxisRendererY.new( root, {} ),
-            } ) );
-
-            const series1 = chart.series.push( am5xy.ColumnSeries.new( root, {
-                "name": "Accepted",
-                "xAxis": xAxis,
-                "yAxis": yAxis,
-                "valueXField": "date",
-                "valueYField": "calls",
-                "fill": "green",
-                "stroke": "green",
-                "stacked": true,
-                "tooltip": am5.Tooltip.new( root, {
-                    "labelText": this.l10nd( "vue-ext", "Calls" ) + ": {valueY}",
-                } ),
-            } ) );
-
-            series1.data.processor = am5.DataProcessor.new( root, {
-                "dateFields": ["date"],
-                "dateFormat": "i",
-            } );
         },
     },
 };
