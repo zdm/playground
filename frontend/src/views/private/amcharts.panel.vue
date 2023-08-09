62246dcbabcc6b27431e4d7f2cbcba2aa24d68fb
zdm <zdm@softvisio.net>
2023-08-09 12:28:18 +0300

    fix: amcharts

--- frontend/src/views/private/amcharts.panel.vue
+++ frontend/src/views/private/amcharts.panel.vue
@@ -1,9 +1,76 @@
 <template>
     <ext-panel/>
+    <Amcharts5 ref="callsChart" :createChart="_createChart" height="150" :updateChart="_updateChart" @refresh="_chartRefresh"/>>
 </template>
 
 <script>
+import Amcharts5 from "#vue/components/amcharts5";
+import * as am5xy from "@amcharts/amcharts5/xy";
+
 export default {
-    "methods": {},
+    "components": { Amcharts5 },
+
+    "methods": {
+        _createChart ( cmp ) {
+            const root = cmp.root,
+                am5 = cmp.am5;
+
+            const chart = root.container.children.push( am5xy.XYChart.new( root, {
+
+                // "panX": true,
+                // "panY": true,
+                // "wheelX": "panX",
+                // "wheelY": "zoomX",
+                // "pinchZoomX": true,
+            } ) );
+
+            chart.set(
+                "cursor",
+                am5xy.XYCursor.new( root, {
+                    "behavior": "none", // "zoomX",
+                } )
+            );
+
+            chart.children.push( am5.Label.new( root, {
+                "text": this.l10nd( "vue-ext", "Calls for the last 60 minutes" ),
+                "fontSize": 12,
+                "x": am5.percent( 50 ),
+                "centerX": am5.percent( 50 ),
+            } ) );
+
+            const xAxis = chart.xAxes.push( am5xy.DateAxis.new( root, {
+                "baseInterval": {
+                    "timeUnit": "minute",
+                    "count": 1,
+                },
+                "renderer": am5xy.AxisRendererX.new( root, {} ),
+                "tooltipDateFormat": "HH:mm",
+                "tooltip": am5.Tooltip.new( root, {} ),
+            } ) );
+
+            const yAxis = chart.yAxes.push( am5xy.ValueAxis.new( root, {
+                "renderer": am5xy.AxisRendererY.new( root, {} ),
+            } ) );
+
+            const series1 = chart.series.push( am5xy.ColumnSeries.new( root, {
+                "name": "Accepted",
+                "xAxis": xAxis,
+                "yAxis": yAxis,
+                "valueXField": "date",
+                "valueYField": "calls",
+                "fill": "green",
+                "stroke": "green",
+                "stacked": true,
+                "tooltip": am5.Tooltip.new( root, {
+                    "labelText": this.l10nd( "vue-ext", "Calls" ) + ": {valueY}",
+                } ),
+            } ) );
+
+            series1.data.processor = am5.DataProcessor.new( root, {
+                "dateFields": ["date"],
+                "dateFormat": "i",
+            } );
+        },
+    },
 };
 </script>
