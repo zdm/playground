af4f513d76b4e6a804e23b370ccfa62e179d682b
zdm <zdm@softvisio.net>
2023-09-03 08:01:09 +0300

    fix: defineAsyncComponent removed

--- frontend/src/viewport.vue
+++ frontend/src/viewport.vue
@@ -1,13 +1,12 @@
 <script>
 import Viewport from "#vue/viewport";
-import { defineAsyncComponent } from "vue";
 
 export default {
     "extends": Viewport,
 
     created () {
-        this.publicView = defineAsyncComponent( () => import( /* webpackChunkName: "public" */ "@/views/public.vue" ) );
-        this.privateView = defineAsyncComponent( () => import( /* webpackChunkName: "private" */ "@/views/private.vue" ) );
+        this.publicView = () => import( /* webpackChunkName: "public" */ "@/views/public.vue" );
+        this.privateView = () => import( /* webpackChunkName: "private" */ "@/views/private.vue" );
     },
 
     "methods": {
