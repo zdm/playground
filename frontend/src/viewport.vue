cd13ee49b40a66e9142bafd31e5145302bb2f879
zdm <zdm@softvisio.net>
2023-11-24 03:49:42 +0200

    fix: telegram

--- frontend/src/viewport.vue
+++ frontend/src/viewport.vue
@@ -12,8 +12,8 @@ export default {
     },
 
     "methods": {
-        async _onRoute ( route ) {
-            return Viewport.methods._onRoute.call( this, route );
+        async _route () {
+            return Viewport.methods._route.call( this );
         },
     },
 };
