fe2be55f26c671ae0ec85daca0850c383357d90b
zdm <zdm@softvisio.net>
2024-06-18 05:30:44 +0300

    fix: translations

--- frontend/src/telegram-test-bot/panel.vue
+++ frontend/src/telegram-test-bot/panel.vue
@@ -1,12 +1,12 @@
 <template>
     <ext-tabpanel defaults='{"padding":"0 0 0 1"}' flex="1" layout='{"animation":{"direction":"vertical","type":"slide"}}' margin="1 0 0  0" tabBar='{"defaults":{"flex":null,"style":"font-size:.7em","textAlign":"center","width":120},"layout":{"align":"start","pack":"start","type":"vbox"}}' tabBarPosition="left" tabRotation="none" viewModel="true" @ready="_ready">
         <!-- info tab -->
-        <ext-panel iconCls="fa-brands fa-telegram" layout="fit" :title="l10n(`Bot info`)">
+        <ext-panel iconCls="fa-brands fa-telegram" layout="fit" :title="l10n(`Info`)">
             <BotDetailsPanel :telegramBotId="telegramBotId"/>
         </ext-panel>
 
         <!-- users -->
-        <ext-panel iconCls="fa-solid fa-users" layout="fit" :title="l10n(`Bot users`)">
+        <ext-panel iconCls="fa-solid fa-users" layout="fit" :title="l10n(`Users`)">
             <BotUsersPanel :telegramBotId="telegramBotId"/>
         </ext-panel>
     </ext-tabpanel>
